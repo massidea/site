59bbd228d76312ae345579176033a56531577875
mixu666 <mixu666@641c7911-cd48-48cd-9293-891b56114118>
2010-05-10 09:45:40 +0000

    Fixed sql fixes.

--- sql/03_small_fixes.sql
+++ sql/03_small_fixes.sql
@@ -1,8 +1,8 @@
 USE `oibs`;
 
---- Fix a typo.
-ALTER TABLE `campaigns_cmp` CHANGE `descsiption_cmp` `description_cmp` TEXT CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL 
+-- Fix a typo.
+ALTER TABLE `campaigns_cmp` CHANGE `descsiption_cmp` `description_cmp` TEXT CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL;
 
---- Move body_grp after description_grp.
-ALTER TABLE `usr_groups_grp` CHANGE `body_grp` `body_grp` TEXT CHARACTER SET latin1 COLLATE latin1_swedish_ci  NOT NULL AFTER `description_grp`
-ALTER TABLE `usr_groups_grp` CHANGE `has_blog_grp` `has_blog_grp` TINYINT( 1 ) NOT NULL AFTER body_grp
+-- Move body_grp after description_grp.
+ALTER TABLE `usr_groups_grp` CHANGE `body_grp` `body_grp` TEXT CHARACTER SET latin1 COLLATE latin1_swedish_ci  NOT NULL AFTER `description_grp`;
+ALTER TABLE `usr_groups_grp` CHANGE `has_blog_grp` `has_blog_grp` TINYINT( 1 ) NOT NULL AFTER body_grp;
