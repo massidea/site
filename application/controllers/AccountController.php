de41995da0e01538eb34cbd1542721450e9d167f
iiuusit <iiro.uusitalo@samk.fi>
2010-09-03 15:38:52 +0300

    Little changes in comments

--- application/controllers/AccountController.php
+++ application/controllers/AccountController.php
@@ -16,9 +16,7 @@
  * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  * License text found in /license/
- */
-
-/**
+ *
  *  AccountController - class
  *
  *  @package     controllers
