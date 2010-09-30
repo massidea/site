2097e7564f032d97106659d6da8eb09aa89e27b7
Jussi Raitanen <jussi.raitanen@student.samk.fi>
2010-09-30 16:34:46 +0300

    Fixes saving and publishing bug; After pressing publish and immediately save, _POST variable still kept published parameter.

--- www/js/ContentForm.js
+++ www/js/ContentForm.js
@@ -148,6 +148,7 @@ $(document).ready(function() {
 		} else if($(this).attr('id') == "content_save_button") {
 			canExit = 1;
 			window.onbeforeunload = null;
+			$("#content_publish").val('');
 			$("#content_save").val('1');
 			$('.content_manage_button').attr('disabled', 'disabled');
 			$('#form_content_realcontent').has(this).children('form').submit();
