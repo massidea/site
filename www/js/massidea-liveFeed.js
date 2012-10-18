c7a53d3391b839e19fbba5f7ceadc1823c24b67c
Michael Lang <S1010307072@students.fh-hagenberg.at>
2012-10-18 10:03:26 +0200

    006 Live Feed Aktualisierung Live Feed

--- www/js/massidea-liveFeed.js
+++ www/js/massidea-liveFeed.js
@@ -1,29 +1,25 @@
-$(function (){
+var $liveFeed = $(".startPageFeed");
+var $liveFeedContent = $(".feedContent");
+var $liveFeedControl = $(".feedControl");
+var time = 0;
 
-    var $liveFeed = $(".startPageFeed");
-    var $liveFeedContent = $(".feedContent");
-    var $liveFeedControl = $(".feedControl");
-    var time = 0;
-
-    $liveFeed.hover(stopTimer, resetTimer);
-    $liveFeedControl.click(loadContent);
-    resetTimer();
+$liveFeed.hover(stopTimer, resetTimer);
+$liveFeedControl.click(loadContent);
+resetTimer();
 
 
-    function loadContent(){
-        $liveFeedContent.load('/'+LANGUAGE+'/content/feed/format/html');
-    }
+function loadContent(){
+    $liveFeedContent.load('/'+LANGUAGE+'/content/feed/format/html');
+}
 
-    function resetTimer(){
-        if (time)
-            clearTimeout(time);
-        time = setTimeout(loadContent, 3000);
-    }
+function resetTimer(){
+    if (time)
+        clearTimeout(time);
+    time = setTimeout(loadContent, 3000);
+}
 
-    function stopTimer(){
-        if (time)
-            clearTimeout(time);
-        time = 0;
-    }
-
-});
+function stopTimer(){
+    if (time)
+        clearTimeout(time);
+    time = 0;
+}
