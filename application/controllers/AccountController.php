1851bc08bacc2ad51b6fb79f2cd81b137493c222
mchalupar <s1010307052@students.fh-hagenberg.at>
2012-11-13 16:09:04 +0100

    Testserver test

--- application/controllers/AccountController.php
+++ application/controllers/AccountController.php
@@ -956,6 +956,7 @@ class AccountController extends Oibs_Controller_CustomController
                     //exit;
 
                     // send verification email
+                    var_dump($this->view->language);
                     if ($user->sendVerificationEmail($userId, $email, $url, $this->view->language->toString())) {
                         $action = 'emailsent';
                         $this->_flashMessenger->addMessage('account-fetchpassword-verification-email-sent-message');
