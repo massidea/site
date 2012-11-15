ff01679176d3c1aff9ed445dde5246a95c10da7c
WHOFBAUER <s0910307060@students.fh-hagenberg.at>
2012-11-15 12:26:14 +0100

    German selection by uri

--- library/Oibs/Controller/Plugin/Language.php
+++ library/Oibs/Controller/Plugin/Language.php
@@ -14,11 +14,12 @@ class Oibs_Controller_Plugin_Language extends Zend_Controller_Plugin_Abstract
 		//$config = Zend_Registry::get('config');
 		
 		// Get languages
-		$locale = new Zend_Locale();		
+		$locale = Zend_Registry::get('Zend_Locale');//new Zend_Locale();
 		//$options = array('scan' => Zend_Translate::LOCALE_FILENAME, 'disableNotices' => true);
 		//$cache = Zend_Registry::get('cache');
 		//Zend_Translate::setCache($cache);
 
+        /*
         $config = Zend_Registry::get('config');
         $languagefiles = $config->language->files;
         //var_dump($languagefiles);
@@ -34,11 +35,11 @@ class Oibs_Controller_Plugin_Language extends Zend_Controller_Plugin_Abstract
                 $translate->addTranslation(APPLICATION_PATH . '/languages/' . $file, $lang);
             }
         }
-
+            */
 
 		//$params = $this->getRequest()->getParams();
 		
-		$language = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+		//$language = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 
         /*
           if(isset($params['language']))
@@ -59,9 +60,9 @@ class Oibs_Controller_Plugin_Language extends Zend_Controller_Plugin_Abstract
           */
 		//else
 		//{
-			$locale->setLocale($language);
-			$translate->setLocale($locale);
-			Zend_Form::setDefaultTranslator($translate);
+			//$locale->setLocale($language);
+			//$translate->setLocale($locale);
+			//Zend_Form::setDefaultTranslator($translate);
 
             //var_dump($translate->getLocale());
         //var_dump(Zend_Translate::LOCALE_FILENAME);
@@ -69,9 +70,51 @@ class Oibs_Controller_Plugin_Language extends Zend_Controller_Plugin_Abstract
 			//setcookie('lang', $locale->getLanguage(), null, '/');
 			
 			Zend_Registry::set('Zend_Locale', $locale);
-			Zend_Registry::set('Zend_Translate', $translate);
-			Zend_Registry::set('Available_Translations', $translate->getList());
+			//Zend_Registry::set('Zend_Translate', $translate);
+			//Zend_Registry::set('Available_Translations', $translate->getList());
 		//}
 	}
+
+    public function routeShutdown(Zend_Controller_Request_Abstract $request)
+    {
+        $locale = new Zend_Locale();
+        //$options = array('scan' => Zend_Translate::LOCALE_FILENAME);
+        if (Zend_Registry::isRegistered('Zend_Translate'))
+            $translate = Zend_Registry::get('Zend_Translate');
+        else
+        {
+            $config = Zend_Registry::get('config');
+            $languagefiles = $config->language->files;
+            //var_dump($languagefiles);
+            //$languagefilesEn = $config->language->files->en->toArray();
+            //var_dump($languagefilesEn);
+            $translate = new Zend_Translate('csv', APPLICATION_PATH . '/languages/index_en.csv', 'en');
+
+            $langArr = $languagefiles->toArray();
+            foreach($langArr as $lang => $files)
+            {
+                foreach ($files as $file)
+                {
+                    $translate->addTranslation(APPLICATION_PATH . '/languages/' . $file, $lang);
+                }
+            }
+        }
+        $requestParams = $this->getRequest()->getParams();
+        $language = (isset($requestParams['language'])) ? $requestParams['language'] : false;
+        if ($language == false) {
+            $language = ($translate->isAvailable($locale->getLanguage())) ? $locale->getLanguage() : 'en';
+        }
+        if (!$translate->isAvailable($language)) {
+            throw new Zend_Controller_Action_Exception('This page dont exist',404);
+        } else {
+            $locale->setLocale($language);
+            $translate->setLocale($locale);
+            Zend_Form::setDefaultTranslator($translate);
+            //setcookie('lang', $locale->getLanguage(), null, '/');
+            Zend_Registry::set('Zend_Locale', $locale);
+            Zend_Registry::set('Zend_Translate', $translate);
+            Zend_Registry::set('Available_Translations', $translate->getList());
+        }
+    }
 }
 ?>
