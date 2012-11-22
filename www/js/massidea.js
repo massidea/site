a442e3a6a43ed1350b8e4e42d7a6d1bd11745162
Lisa Jedinger <lisa.jedinger@gmx.at>
2012-11-22 10:00:45 +0100

    finished mainnavigation layout

--- www/js/massidea.js
+++ www/js/massidea.js
@@ -3,6 +3,8 @@ $('*[rel=popover-hover]').popover({
 	html:'true'
 });
 
+
+
 //language selection
 $("#languageMenu").change(function() {
    var curLan = $(":selected", this).val();
@@ -13,3 +15,27 @@ $("#languageMenu").change(function() {
    }
    location.href = '/' + curLan + '/index/change-language?language=' + curLan + '&returnUrl=' + escape(curLocation);
 });
+
+var popover = null;
+$('.mainnavigation_popover').popover({
+    trigger:'manual',
+    html:'true'
+}).live("click", function () {
+        // open new one
+        if (this != popover) {
+            $(popover).popover('hide');
+            $(this).popover('show');
+            $('.popover-title:not(:has(a))').append('<a class="close">x</a>');
+            popover = this;
+
+        } else { // close
+            $(this).popover('hide');
+            popover = null;
+        }
+
+    });
+$('.popover .close').live('click', function() {
+    $(popover).popover('hide');
+    popover = null;
+});
+
