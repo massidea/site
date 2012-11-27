4fa8c48bb39d74d983ca96fb399964b4179bb62f
Jan Michael Auer <jan.auer@me.com>
2012-11-27 17:19:05 +0100

    Implemented content loading and filtering on client side.

--- www/js/massidea-liveFeed.js
+++ www/js/massidea-liveFeed.js
@@ -4,40 +4,41 @@ var $liveFeedControl = $(".feedControl");
 var $feedOverlay = $(".feedOverlay");
 var time = 0;
 
-    //stop reloading of the page
-    $liveFeedControl.click(function (event) {
-        event.stopPropagation();
-        return false;
-    });
+//stop reloading of the page
+$liveFeedControl.click(function (event) {
+	event.stopPropagation();
+	return false;
+});
 
-    $liveFeed.hover(stopTimer, resetTimer);
-    $liveFeedControl.click(loadContentClick);
-    loadContent();
-    resetTimer();
+$liveFeed.hover(stopTimer, resetTimer);
+$liveFeedControl.click(loadContentClick);
+loadContent();
+resetTimer();
 
-    function loadContentClick(){
-        $feedOverlay.removeClass("hidden");
-        $liveFeedContent.load('/'+LANGUAGE+'/content/feed/format/html', function (){
-            $feedOverlay.addClass("hidden");
-    });
+function loadContentClick () {
+	$feedOverlay.removeClass("hidden");
+	$liveFeedContent.load('/' + LANGUAGE + '/content/feed/format/html', function () {
+		$feedOverlay.addClass("hidden");
+	});
 }
-    function loadContent(){
-        $feedOverlay.removeClass("hidden");
-        $liveFeedContent.load('/'+LANGUAGE+'/content/feed/format/html', function (){
-            $feedOverlay.addClass("hidden");
-            resetTimer();
-        });
-    }
+function loadContent () {
+	$feedOverlay.removeClass("hidden");
+	$liveFeedContent.load('/' + LANGUAGE + '/content/feed/format/html', function () {
+		$feedOverlay.addClass("hidden");
+		resetTimer();
+	});
+}
 
-    function resetTimer(){
-        if (time) {
-            window.clearTimeout(time);
-        }
-        time = window.setTimeout(loadContent, 15000);
-    }
+function resetTimer () {
+	if (time) {
+		window.clearTimeout(time);
+	}
+	time = window.setTimeout(loadContent, 15000);
+}
 
-    function stopTimer(){
-        if (time)
-            window.clearTimeout(time);
-        time = 0;
-    }
+function stopTimer () {
+	if (time) {
+		window.clearTimeout(time);
+	}
+	time = 0;
+}
