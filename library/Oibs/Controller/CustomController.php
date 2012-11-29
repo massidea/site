01701e7c1f16665bea45a648977f37f36faceea3
Jürgen <s1010307088@students.fh-hagenberg.at>
2012-11-29 11:09:05 +0100

    034 mainmenu view und controller in layout und custom_controller integriert

--- library/Oibs/Controller/CustomController.php
+++ library/Oibs/Controller/CustomController.php
@@ -50,6 +50,7 @@ class Oibs_Controller_CustomController extends Zend_Controller_Action
 	protected $_redirector;
 	protected $_flashMessenger;
     protected $_urlHelper;
+    protected $_mainnavHelper;
 	protected $_identity;
 
 	/**
@@ -194,9 +195,50 @@ class Oibs_Controller_CustomController extends Zend_Controller_Action
 
         $this->view->activeLanguages = $activeLanguages;
 
-
+        $this->mainNavigation();
 
     } // end of init
+
+    public function mainNavigation() {
+        $id = -1;
+        if (Zend_Controller_Action_HelperBroker::hasHelper('redirector')) {
+            $redirector = Zend_Controller_Action_HelperBroker::getExistingHelper('redirector');
+        }
+        $auth = Zend_Auth::getInstance();
+        if($auth->hasIdentity()){
+            $identity = $auth->getIdentity();
+            $id = $identity->user_id;
+        } else {
+            $target = $this->_urlHelper->url(array('controller' => 'index',
+                    'action' => 'index',
+                    'language' => $this->view->language),
+                'lang_default', true);
+            $redirector->gotoUrl($target);
+        }
+
+        $userGroups = null;
+        $categories = null;
+
+        $categoryModel = null;
+        $userModel = null;
+
+        if($id != -1){
+            $userModel = new Default_Model_User();
+            $categoryModel = new Default_Model_Category();
+
+            $userGroups = $userModel->getUserGroups($id);
+            $categories = $categoryModel->getCategories();
+        }
+        $this->view->groups = $userGroups;
+        $this->view->categories = $categories;
+        /*$userCompaigns = null;
+        if($this->id != -1){
+            $userModel = new Default_Model_User();
+            $userCompaigns = $userModel->getUserCampaigns($this->id);
+        }
+        $this->view->campaigns = $userCompaigns;
+        */
+    }
 
     /**
      * change Lanugage Action
